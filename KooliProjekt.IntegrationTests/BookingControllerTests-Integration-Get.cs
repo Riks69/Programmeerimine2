2d47da48f15b1a1a709cc7227aee5c756fa78c46
Riks <Rikoarti.arju@gmail.com@KLASSA302-22>
2025-05-22 14:11:00 +0300

    Get Integration

---
+++ KooliProjekt.IntegrationTests/BookingControllerTests-Integration-Get.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KooliProjekt.Data;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class BookingControllerTests : TestBase
+    {
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public BookingControllerTests()
+        {
+            _client = Factory.CreateClient();
+            _context = (ApplicationDbContext)Factory.Services.GetService(typeof(ApplicationDbContext));
+        }
+
+        [Fact]
+        public async Task Index_should_return_correct_response()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Bookings");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_was_not_found()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Bookings/Details/100");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Bookings/Details/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_ok_when_list_was_found()
+        {
+            // Arrange
+            var list = new Booking { UserId = 1, StartTime = new DateTime(2024, 11, 25, 9, 0, 0), EndTime = new DateTime(2024, 11, 28, 9, 0, 0), DistanceKm = 200, IsCompleted = true };
+            _context.Bookings.Add(list);
+            _context.SaveChanges();
+
+            // Act
+            using var response = await _client.GetAsync("/Bookings/Details/" + list.Id);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ KooliProjekt.IntegrationTests/InvoiceControllerTests-Integration-Get.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using KooliProjekt.Data;
+using KooliProjekt.IntegrationTests.Helpers;
+using Xunit;
+using static NuGet.Packaging.PackagingConstants;
+
+namespace KooliProjekt.IntegrationTests
+{
+    [Collection("Sequential")]
+    public class InvoiceControllerTests : TestBase
+    {
+        private readonly HttpClient _client;
+        private readonly ApplicationDbContext _context;
+
+        public InvoiceControllerTests()
+        {
+            _client = Factory.CreateClient();
+            _context = (ApplicationDbContext)Factory.Services.GetService(typeof(ApplicationDbContext));
+        }
+
+        [Fact]
+        public async Task Index_should_return_correct_response()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Invoices");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_list_was_not_found()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Invoices/Details/100");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_notfound_when_id_is_missing()
+        {
+            // Arrange
+
+            // Act
+            using var response = await _client.GetAsync("/Invoices/Details/");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task Details_should_return_ok_when_list_was_found()
+        {
+            // Arrange
+            var list = new Invoice { BookingId = 10, Amount = 44, Description = "j", IsPaid = true };
+            _context.Invoices.Add(list);
+            _context.SaveChanges();
+
+            // Act
+            using var response = await _client.GetAsync("/Invoices/Details/" + list.Id);
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
